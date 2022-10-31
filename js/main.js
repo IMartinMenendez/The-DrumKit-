187d14315e3c77ddb6a0216624c78298536b7a40
Irene Martin <79365505+IMartinMenendez@users.noreply.github.com>
2022-10-31 19:35:55 +0000

    trying to fix issue with sound 2

--- js/main.js
+++ js/main.js
@@ -1,48 +1,60 @@
 document.getElementById("crash-cymbal").addEventListener("click", () => {
-  new Audio("../assets/crash.wav").play().then(r =>
-    r
-  );
-})
-
+    new Audio("../assets/crash.wav").play().then(() => {
+        // Audio is playing.
+    }).catch(error => {
+        console.log(error);
+    });
+});
 document.getElementById("hi-hat-cymbal").addEventListener("click", () => {
-  new Audio("../assets/hi-hat.wav").play().then(r =>
-      r
-  );
-})
-
+    new Audio("../assets/hi-hat.wav").play().then(() => {
+        // Audio is playing.
+    }).catch(error => {
+        console.log(error);
+    });
+});
 document.getElementById("tom-drums1").addEventListener("click", () => {
-  new Audio("../assets/high-tom.wav").play().then(r =>
-      r
-  );
-})
+    new Audio("../assets/high-tom.wav").play().then(() => {
+        // Audio is playing.
+    }).catch(error => {
+        console.log(error);
+    });
+});
 
 document.getElementById("snare-drum").addEventListener("click", () => {
-  new Audio("../assets/snare.wav").play().then(r =>
-      r
-  );
-})
+    new Audio("../assets/snare.wav").play().then(() => {
+        // Audio is playing.
+    }).catch(error => {
+        console.log(error);
+    });
+});
 
 document.getElementById("bass-kick-drum").addEventListener("click", () => {
-  new Audio("../assets/bass-drum.wav").play().then(r =>
-      r
-  );
-})
+    new Audio("../assets/bass-drum.wav").play().then(() => {
+        // Audio is playing.
+    }).catch(error => {
+        console.log(error);
+    });
+});
 
 document.getElementById("ride-cymbal").addEventListener("click", () => {
-  new Audio("../assets/ride.wav").play().then(r =>
-      r
-  );
-})
+    new Audio("../assets/ride.wav").play().then(() => {
+        // Audio is playing.
+    }).catch(error => {
+        console.log(error);
+    });
+});
 
 document.getElementById("tom-drums2").addEventListener("click", () => {
-  new Audio("../assets/high-tom.wav").play().then(r =>
-      r
-  );
-})
+    new Audio("../assets/high-tom.wav").play().then(() => {
+    }).catch(error => {
+        console.log(error);
+    });
+});
 
 document.getElementById("floor-tom-drum").addEventListener("click", () => {
-  new Audio("../assets/medium-tom.wav").play().then(r =>
-      r
-  );
-})
+    new Audio("../assets/medium-tom.wav").play().then(() => {
+    }).catch(error => {
+        console.log(error);
+    });
+});
 

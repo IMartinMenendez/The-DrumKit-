d6da6f4e524e0159f976496d9e8bca7d049a4d27
Irene Martin <79365505+IMartinMenendez@users.noreply.github.com>
2022-10-31 19:19:52 +0000

    trying to fix issue with sound

--- js/main.js
+++ js/main.js
@@ -1,32 +1,48 @@
 document.getElementById("crash-cymbal").addEventListener("click", () => {
-  new Audio("../assets/crash.wav").play();
+  new Audio("../assets/crash.wav").play().then(r =>
+    r
+  );
 })
 
 document.getElementById("hi-hat-cymbal").addEventListener("click", () => {
-  new Audio("../assets/hi-hat.wav").play();
+  new Audio("../assets/hi-hat.wav").play().then(r =>
+      r
+  );
 })
 
 document.getElementById("tom-drums1").addEventListener("click", () => {
-  new Audio("../assets/high-tom.wav").play();
+  new Audio("../assets/high-tom.wav").play().then(r =>
+      r
+  );
 })
 
 document.getElementById("snare-drum").addEventListener("click", () => {
-  new Audio("../assets/snare.wav").play();
+  new Audio("../assets/snare.wav").play().then(r =>
+      r
+  );
 })
 
 document.getElementById("bass-kick-drum").addEventListener("click", () => {
-  new Audio("../assets/bass-drum.wav").play();
+  new Audio("../assets/bass-drum.wav").play().then(r =>
+      r
+  );
 })
 
 document.getElementById("ride-cymbal").addEventListener("click", () => {
-  new Audio("../assets/ride.wav").play();
+  new Audio("../assets/ride.wav").play().then(r =>
+      r
+  );
 })
 
 document.getElementById("tom-drums2").addEventListener("click", () => {
-  new Audio("../assets/high-tom.wav").play();
+  new Audio("../assets/high-tom.wav").play().then(r =>
+      r
+  );
 })
 
 document.getElementById("floor-tom-drum").addEventListener("click", () => {
-  new Audio("../assets/medium-tom.wav").play();
+  new Audio("../assets/medium-tom.wav").play().then(r =>
+      r
+  );
 })
 

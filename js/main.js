83b973e71591d6fd4ae43d4b2f7d637a44238208
Irene Martin <79365505+IMartinMenendez@users.noreply.github.com>
2022-10-31 18:48:21 +0000

    initial commit and project completed

---
+++ js/main.js
@@ -0,0 +1,32 @@
+document.getElementById("crash-cymbal").addEventListener("click", () => {
+  new Audio("../assets/crash.wav").play();
+})
+
+document.getElementById("hi-hat-cymbal").addEventListener("click", () => {
+  new Audio("../assets/hi-hat.wav").play();
+})
+
+document.getElementById("tom-drums1").addEventListener("click", () => {
+  new Audio("../assets/high-tom.wav").play();
+})
+
+document.getElementById("snare-drum").addEventListener("click", () => {
+  new Audio("../assets/snare.wav").play();
+})
+
+document.getElementById("bass-kick-drum").addEventListener("click", () => {
+  new Audio("../assets/bass-drum.wav").play();
+})
+
+document.getElementById("ride-cymbal").addEventListener("click", () => {
+  new Audio("../assets/ride.wav").play();
+})
+
+document.getElementById("tom-drums2").addEventListener("click", () => {
+  new Audio("../assets/high-tom.wav").play();
+})
+
+document.getElementById("floor-tom-drum").addEventListener("click", () => {
+  new Audio("../assets/medium-tom.wav").play();
+})
+
